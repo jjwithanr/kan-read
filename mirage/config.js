7eb5375c1126fe5861e69e1643eb908170d22f27
jjwithanr <jeremychang623@gmail.com>
2022-12-06 18:59:29 -0800

    Configure backend API CRUD links

--- mirage/config.js
+++ mirage/config.js
@@ -9,18 +9,12 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  this.namespace = 'api';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
+  this.get('/books');
+  this.post('/books');
+  this.get('/books/:id');
+  this.patch('/books/:id');
+  this.del('/books/:id');
 }
