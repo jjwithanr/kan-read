05c977159677462aedaa66c23ab1e817edd85e15
jjwithanr <jeremychang623@gmail.com>
2022-12-09 17:55:39 -0800

    Enable link for editing author on authors page

--- app/models/author.js
+++ app/models/author.js
@@ -2,5 +2,5 @@ import Model, { attr,hasMany } from '@ember-data/model';
 
 export default class AuthorModel extends Model {
     @attr('string') name;
-    @hasMany('book', { inverse: 'author' }) books;
+    @hasMany('book') books;
 }
