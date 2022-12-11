f01a4af19388fdb2408f77930c2e793cd78dcfcf
jjwithanr <jeremychang623@gmail.com>
2022-12-10 19:46:34 -0800

    Implement status attribute for book model

--- app/models/book.js
+++ app/models/book.js
@@ -2,5 +2,7 @@ import Model, { attr,belongsTo } from '@ember-data/model';
 
 export default class BookModel extends Model {
     @attr('string') title;
+    @attr('string', { 
+        defaultValue() { return "unread"; }}) status;
     @belongsTo('author') author;
 }
