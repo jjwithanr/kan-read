064680c1899632e4b19581609d5a9fd18545c517
jjwithanr <jeremychang623@gmail.com>
2022-12-13 10:50:26 -0800

    Update author book list when book titled is edited

--- app/controllers/edit.js
+++ app/controllers/edit.js
@@ -1,13 +1,24 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { faBookJournalWhills } from '@fortawesome/free-solid-svg-icons';
 
 export default class EditController extends Controller {
     @action
-    editBook(book, event) {
-        event.preventDefault();
+    editBook(book, books, authors, event) {
+        event.preventDefault();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+        authors.forEach(author => {
+            if (author.name == book.authorName) {
+                author.bookList = "";
+                books.forEach(b => {
+                    if (b.authorName == book.authorName) {
+                        author.bookList += b.title + ", ";
+                    }
+                });
+            }
+        });
 
         let newStatus = document.getElementById('status').value;
-        
         if (newStatus != book.status) {
             book.status = newStatus;
             book.save().then(() => {
