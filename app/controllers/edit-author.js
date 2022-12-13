14792994f25ad47ee1ca1489f8afa567127b9074
jjwithanr <jeremychang623@gmail.com>
2022-12-13 11:42:24 -0800

    Match book author name to newly edited author name

--- app/controllers/edit-author.js
+++ app/controllers/edit-author.js
@@ -3,8 +3,17 @@ import { action } from '@ember/object';
 
 export default class EditAuthorController extends Controller {
     @action
-    editAuthor(author, event) {
+    editAuthor(author, books, event) {
         event.preventDefault();
+
+        let booksList = author.bookList.split(", ");
+        for (let i = 0; i < booksList.length; i++) {
+            books.forEach(book => {
+                if (book.title == booksList[i]) {
+                    book.authorName = author.name;
+                }
+            });
+        }
 
         author.save().then(() => {
             this.transitionToRoute('authors')
--- app/routes/edit-author.js
+++ app/routes/edit-author.js
@@ -1,7 +1,12 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default class EditAuthorRoute extends Route {
     model(params) {
-        return this.store.findRecord('author', params.id);
+        return hash({
+            author: this.store.findRecord('author',  params.id),
+            authors: this.store.findAll('author'),
+            books: this.store.findAll('book')
+        });
     }
 }
