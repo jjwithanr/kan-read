d21b4b7a65a5f4bf97c83af5e2be3b724e403f53
jjwithanr <jeremychang623@gmail.com>
2022-12-13 00:00:49 -0800

    Populate book author relationship with data

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -5,6 +5,23 @@ export default function(server) {
     This data will not be loaded in your tests.
   */
   server.createList('author', 5).forEach(author => {
-    server.createList('book', 3, { author });
+    server.createList('book', 2, { authorName: author.name}).forEach(book => {
+      if (!author.bookList) {
+        author.update({
+          bookList: book.title + ", "
+        });
+      } else {
+        author.update({
+          bookList: author.bookList + book.title + ", "
+        });
+      }
+    });
+    
+    if (author.bookList) {
+      author.update({
+        bookList: author.bookList.slice(0, -2)
+      });
+    }
+
   });
 }
