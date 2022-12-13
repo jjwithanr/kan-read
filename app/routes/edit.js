92009cea5ca4cfdc4526bf8d278524f2956f3b9b
jjwithanr <jeremychang623@gmail.com>
2022-12-13 10:50:26 -0800

    Update author book list when book titled is edited

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
+
 
 export default class EditRoute extends Route {
     model(params) {
-        return this.store.findRecord('book', params.id);
+        return hash({
+            book: this.store.findRecord('book',  params.id),
+            books: this.store.findAll('book'),
+            authors: this.store.findAll('author')
+        });
     }
 }
