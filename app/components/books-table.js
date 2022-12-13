b3fb137a0d702fe7c8ea618058c0928229011070
jjwithanr <jeremychang623@gmail.com>
2022-12-13 11:00:12 -0800

    Remove book from author book list when deleted

--- app/components/books-table.js
+++ app/components/books-table.js
@@ -6,7 +6,23 @@ export default class BooksTableComponent extends Component {
     @tracked query = '';
     
     @action
-    deleteBook(book) {
+    deleteBook(book, books, authors) {
+        
+        authors.forEach(author => {
+            if (author.name == book.authorName) {
+                author.bookList = "";
+                books.forEach(b => {
+                    if (b.authorName == book.authorName) {
+                        if (b == book) {
+                            return;
+                        }
+                        author.bookList += b.title + ", ";
+                    }
+                });
+                author.bookList = author.bookList.slice(0, -2);
+            }
+        });
+
         book.destroyRecord();
     }
 }
