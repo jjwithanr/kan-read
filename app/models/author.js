30f1802f8dc02aab3313f768257496d8b5a7bf66
jjwithanr <jeremychang623@gmail.com>
2022-12-12 23:53:51 -0800

    Allow author model to track books by them

--- app/models/author.js
+++ app/models/author.js
@@ -2,5 +2,6 @@ import Model, { attr,hasMany } from '@ember-data/model';
 
 export default class AuthorModel extends Model {
     @attr('string') name;
+    @attr('string') bookList;
     @hasMany('book') books;
 }
