6dd970c8dde22888419dead71f9cdbb1184069c1
jjwithanr <jeremychang623@gmail.com>
2022-12-13 17:15:32 -0800

    Verify relationship between books and author

--- tests/unit/models/author-test.js
+++ tests/unit/models/author-test.js
@@ -1,13 +1,26 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { get } from '@ember/object';
 
 module('Unit | Model | author', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('author', {});
     assert.ok(model);
+  });
+
+  test('should be written by an author', function(assert) {
+    const book = this.owner.lookup('service:store').modelFor('book');
+
+    const relationship = get(book, 'relationshipsByName').get('author');
+
+    assert.equal(relationship.key, 'author', 'has relationship with author');
+    assert.equal(
+      relationship.kind,
+      'belongsTo',
+      'kind of relationship is belongsTo'
+    );
   });
 });
--- tests/unit/models/book-test.js
+++ tests/unit/models/book-test.js
@@ -1,13 +1,26 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { get } from '@ember/object';
 
 module('Unit | Model | book', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('book', {});
     assert.ok(model);
+  });
+
+  test('should have books', function(assert) {
+    const author = this.owner.lookup('service:store').modelFor('author');
+
+    const relationship = get(author, 'relationshipsByName').get('books');
+
+    assert.equal(relationship.key, 'books', 'has relationship with book');
+    assert.equal(
+      relationship.kind,
+      'hasMany',
+      'kind of relationship is hasMany'
+    );
   });
 });
